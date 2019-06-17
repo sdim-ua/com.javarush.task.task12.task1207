bfc73120dfa6cc585943a56011efef2f22bd2324
sdim-ua <ingersol@yandex.ua>
2019-06-17 23:54:39 +0400

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,24 @@
+/* 
+Int и Integer
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+       Integer x = 5;
+       
+       print(x);
+       
+       int y = 6;
+       
+       print(y);
+    }
+
+    //Напишите тут ваши методы
+    
+     public static void print (int x){
+        System.out.println(x);
+    }
+     public static void print (Integer x){
+        System.out.println(x);
+    }
+}
